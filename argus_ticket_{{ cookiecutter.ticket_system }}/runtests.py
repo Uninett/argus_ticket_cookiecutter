69669ea49526f4a62770f6e1ecb05b6e69432533
Hanne Moa <hanne.moa@sikt.no>
2022-11-18 14:14:47 +0100

    Make tests more verbose

--- argus_ticket_{{ cookiecutter.ticket_system }}/runtests.py
+++ argus_ticket_{{ cookiecutter.ticket_system }}/runtests.py
@@ -10,8 +10,9 @@ from django.test.utils import get_runner
 
 if __name__ == "__main__":
     from django import setup
+
     setup()
     TestRunner = get_runner(settings)
-    test_runner = TestRunner()
+    test_runner = TestRunner(verbosity=2)
     failures = test_runner.run_tests(["tests"])
     sys.exit(bool(failures))
