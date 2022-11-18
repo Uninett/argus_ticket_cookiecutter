8d16007268604f26618e5aee9ad581e41c317534
Hanne Moa <hanne.moa@sikt.no>
2022-11-18 14:14:19 +0100

    Update test settings for newest argus_server

--- argus_ticket_{{ cookiecutter.ticket_system }}/tests/test_settings.py
+++ argus_ticket_{{ cookiecutter.ticket_system }}/tests/test_settings.py
@@ -1,4 +1,4 @@
-SECRET_KEY = 'stuffandnonsense'
+SECRET_KEY = "stuffandnonsense"
 
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
@@ -7,22 +7,26 @@ AUTH_USER_MODEL = "argus_auth.User"
 USE_TZ = True
 
 INSTALLED_APPS = [
-#     'django.contrib.contenttypes',
-#     'django.contrib.auth',
-# 
-#     # Must make tables
-#     'argus.auth',
-#     'argus.incident',
-#     'argus.notificationprofile',
+    "django.contrib.contenttypes",
+    "django.contrib.auth",
+
+    # Must make tables
+    "rest_framework.authtoken",
+    "argus.auth",
+    "argus.incident",
+    "argus.notificationprofile",
 ]
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': ':memory:',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
     },
 }
 
 MIDDLEWARE_CLASSES = ()
 
-# Argus ticket specific settings
+# Argus specific settings
+MEDIA_PLUGINS = [
+    "argus.notificationprofile.media.email.EmailNotification",
+]
